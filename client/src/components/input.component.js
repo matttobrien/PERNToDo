d0f887f32a05d1671a662c9e7e4a6ceaf4b86a4c
Matthew O'Brien <mobrien1799@gmail.com>
2021-09-22 16:46:04 -0400

    fixed missing reload when adding

--- client/src/components/input.component.js
+++ client/src/components/input.component.js
@@ -11,6 +11,7 @@ const Input = () => {
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(body)
       });
+      window.location = "/";
     } catch (error) {
       console.error(error);
     }
